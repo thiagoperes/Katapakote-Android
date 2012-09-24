2c48b3f0bb813176d194b7278f635c2a58d1d36b
thiagoperes <thiago@appkraft.net>
2012-09-24 16:38:56 -0300

    new icon, interface changes, custom cell added

--- src/com/example/listactivity/MainActivity.java
+++ src/com/example/listactivity/MainActivity.java
@@ -1,42 +1,47 @@
 package com.example.listactivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.markupartist.android.widget.PullToRefreshListView;
+import com.markupartist.android.widget.PullToRefreshListView.OnRefreshListener;
 
 public class MainActivity extends ListActivity {
 	
 	private static final int DETAIL_ACTIVITY = 1;
 	private static final int ADD_ACTIVITY = 2;
-	private static ArrayAdapter<String> adapter;
 	
 	private void refreshPackageList() {
 		SessionManager.initDB(this.getApplicationContext());
 		
-		if (adapter == null)
-		{
-			adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SessionManager.getAllPackages());
-			setListAdapter(adapter);
-			return;
-		}
-		adapter.clear();
-		
-		for (String s : SessionManager.getAllPackages())
-		{
-			adapter.add(s);
-		}
+		setListAdapter(new ListAdapter(this, SessionManager.getAllPackages()));
 	}
 	
     @Override
@@ -44,14 +49,13 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        SessionManager.postRequest();
-        
+                
         refreshPackageList();
+        
+        SessionManager.requestForPackage("RU467405286CH");
         
         this.getListView().setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
 					long id) {
 				// TODO Auto-generated method stub
@@ -66,7 +70,6 @@ public class MainActivity extends ListActivity {
         Button btn = (Button)findViewById(R.id.button1);
         btn.setOnClickListener(new OnClickListener() {
 			
-			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent i = new Intent(MainActivity.this, AddPackageActivity.class);
@@ -79,8 +82,7 @@ public class MainActivity extends ListActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
       if (v.getId() == this.getListView().getId()) 
       {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
-        //menu.setHeaderTitle("Opções");
+        //AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
         menu.add(Menu.NONE, 0, 0, "Remover pakote");
       }
     }
@@ -90,7 +92,8 @@ public class MainActivity extends ListActivity {
       AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
       int menuItemIndex = info.position;
             
-      String trackingCode = getListAdapter().getItem(menuItemIndex).toString();
+      ListAdapter tmpAdapter = (ListAdapter)getListAdapter();
+      String trackingCode = tmpAdapter.getItem(menuItemIndex).get("trackingCode");
       
       if (SessionManager.removePackage(trackingCode) == 0)
       {
@@ -111,5 +114,65 @@ public class MainActivity extends ListActivity {
     	{
     		refreshPackageList();
     	}
+    }
+    
+    private class ListAdapter extends BaseAdapter {
+    	private Context context; // needed to create the view
+    	private ArrayList<HashMap<String, String>> packages;
+    	
+		public ListAdapter(Context c, ArrayList<HashMap<String, String>> list) {
+			context = c;
+			packages = list;
+		}
+
+		public int getCount() {
+			return packages.size(); // TODO: Implementar contador
+		}
+
+		public HashMap<String, String> getItem(int position) {
+			return packages.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			ViewHolder vh;
+
+			if (convertView == null) {
+
+				vh = new ViewHolder();
+				convertView = LayoutInflater.from(context).inflate(
+						R.layout.package_cell, null);
+				vh.packageName = (TextView)convertView.findViewById(R.id.packageNameTV);
+				vh.trackingCode = (TextView)convertView.findViewById(R.id.trackingCodeTV);
+				vh.status = (TextView)convertView.findViewById(R.id.statusTV);
+				vh.address = (TextView)convertView.findViewById(R.id.addressTV);
+
+				convertView.setTag(vh);
+
+			} else {
+				vh = (ViewHolder) convertView.getTag();
+			}
+
+			// acessa e coloca os valores
+			HashMap<String, String> tmp = getItem(position);
+			
+			vh.packageName.setText(tmp.get("name").toString());
+			vh.trackingCode.setText(tmp.get("trackingCode").toString());
+			vh.status.setText(tmp.get("status").toString());
+			vh.address.setText(tmp.get("address").toString());
+			
+			return convertView;
+		}
+
+		class ViewHolder {
+			TextView packageName;
+			TextView trackingCode;
+			TextView status;
+			TextView address;
+		}
     }
 }
--- src/com/example/listactivity/SessionManager.java
+++ src/com/example/listactivity/SessionManager.java
@@ -1,13 +1,21 @@
 package com.example.listactivity;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.util.Xml;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -16,6 +24,7 @@ import com.loopj.android.http.RequestParams;
 public class SessionManager {
 	private static DB dbHelper;
 	private static AsyncHttpClient client = new AsyncHttpClient();
+	private static final String BASE_URL = "http://services.encomendaz.net/tracking.json?id=";
 	
 	static void initDB(Context context) {
 		if (dbHelper == null)
@@ -37,17 +46,23 @@ public class SessionManager {
         }
 	}
 	
-	static ArrayList<String> getAllPackages() {
+	static ArrayList<HashMap<String, String>> getAllPackages() {
 
         SessionManager.startDB();
         
-        ArrayList<String> packages = new ArrayList<String>();
+        ArrayList<HashMap<String, String>> packages = new ArrayList<HashMap<String, String>>();
         
         Cursor c = dbHelper.getPackages();
         c.moveToFirst();
         while(!c.isAfterLast())
         {
-        	packages.add(c.getString(1));
+        	HashMap<String, String> tmp = new HashMap<String, String>();
+        	tmp.put("trackingCode", c.getString(1));
+        	tmp.put("name", c.getString(2));
+        	tmp.put("status", "vamos l‡");
+        	tmp.put("address", "uhul");
+        	packages.add(tmp);
+        	
         	c.moveToNext();
         }
         c.close();
@@ -72,22 +87,38 @@ public class SessionManager {
 		
 		return ret;
 	}
-	
-	static void postRequest() {
-		RequestParams params = new RequestParams();
-		params.put("Usuario", "KRAFT12345");
-		params.put("Senha", "PUVR3AS1=D");
-		params.put("Tipo", "L");
-		params.put("Resultado", "T");
-		params.put("Objetos", "RA650936674CN");
 		
-		System.out.println("REQUEST STARTED");
-		client.post("http://websro.correios.com.br/sro_bin/sroii_xml.eventos", params, new AsyncHttpResponseHandler() {
+	static void parseRequest(String requestResponse)
+	{
+		try {
+			JSONObject obj = new JSONObject(requestResponse);
+			System.out.println(obj);
+			JSONArray statuses = obj.getJSONArray("data");
+			
+			if (statuses.length() == 0) return;
+			
+			// open db connection
+			for (int i = 0; i < statuses.length(); i++) 
+			{
+				
+			}
+			// close db connection
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	static void requestForPackage(String trackingCode) {	
+		
+		System.out.println("hello");
+		
+		client.get(BASE_URL + trackingCode, new AsyncHttpResponseHandler() {
 		    @Override
 		    public void onSuccess(String response) {
 		        System.out.println(response);
-		        
-		        
+		        parseRequest(response);
 		    }
 		    @Override
 		     public void onFailure(Throwable e, String response) {
@@ -100,5 +131,9 @@ public class SessionManager {
 		         // Completed the request (either success or failure)
 		     }
 		});
+	}
+	
+	static void refreshAllPackages() {
+		
 	}
 }
